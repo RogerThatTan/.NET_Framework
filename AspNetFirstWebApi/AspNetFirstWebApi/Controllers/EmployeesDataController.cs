cc5b0ebc58e33f4d38496348312ae1ea04202759
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2024-12-28 23:14:26 +0600

    api

---
+++ AspNetFirstWebApi/AspNetFirstWebApi/Controllers/EmployeesDataController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AspNetFirstWebApi.Controllers
+{
+    public class EmployeesDataController : ApiController
+    {
+        public string[] myEmployees =
+        {
+            "John Doe",
+            "Jane Doe",
+            "John Smith",
+            "Jane Smith"
+        };
+
+
+        [HttpGet]
+        public string[] GetEmployees()    //will return the arrays of employee
+        { 
+            return myEmployees;
+        }
+
+        [HttpGet]
+        public string GetEmployeeByIndex(int id) 
+        {
+            return myEmployees[id];
+        }
+
+    }
+}
