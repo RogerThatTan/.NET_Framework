605778be3a73e8ce8dde7108cb27301d57f653ae
Tanvir Hassan <22-46481-1@student.aiub.edu>
2025-01-08 13:36:03 +0600

    Interace

---
+++ News Tier Based with Dependency/BLL/Services/NewsService.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DAL.EF;
+using DAL.Interfaces;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class NewsService
+    {   
+       static INewsRepo solid = DataAccess.StudentDataAccess();
+        public static Mapper GetMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<News, NewsDTO>();
+                cfg.CreateMap<NewsDTO, News>();
+            });
+            var mapper = new Mapper(config);
+            return mapper;
+        }
+
+        public static void Create(NewsDTO n) 
+        {
+             solid.Create(GetMapper().Map<News>(n));
+        }
+
+        public static List<NewsDTO> GetAll()
+        {
+            return GetMapper().Map<List<NewsDTO>>(solid.GetAll());
+        }
+
+        public static NewsDTO GetNews(int id)
+        {
+            return GetMapper().Map<NewsDTO>(solid.GetNews(id));
+        }
+
+        public static void Delete(int id)
+        {
+            solid.Delete(id);
+        }
+
+        public static void Update(NewsDTO n)
+        {
+            solid.Update(GetMapper().Map<News>(n));
+        }
+
+        public static List<NewsDTO> NewsByTitle(string title)
+        {
+            return GetMapper().Map<List<NewsDTO>>(solid.NewsByTitle(title));
+        }
+
+        public static List<NewsDTO> NewsByCategory(string title)
+        {
+            return GetMapper().Map<List<NewsDTO>>(solid.NewsByCategory(title));
+        }
+        public static List<NewsDTO> NewsByDate(DateTime date)
+        {
+            return GetMapper().Map<List<NewsDTO>>(solid.NewsByDate(date));
+        }
+
+        public static List<NewsDTO> NewsByDateCat(DateTime date, string category)
+        {
+            return GetMapper().Map<List<NewsDTO>>(solid.NewsByDateCat(date, category));
+        }
+
+        public static List<NewsDTO> NewsByDateTitle(DateTime date, string title)
+        {
+            return GetMapper().Map<List<NewsDTO>>(solid.NewsByDateCat(date,title));
+        }
+
+    }
+}
---
+++ News Tier Based with Dependency/DAL/Repos/NewsRepo.cs
@@ -0,0 +1,80 @@
+﻿using DAL.EF;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class NewsRepo : Repo,INewsRepo
+    {
+        public  void Create(News n)
+        {
+            db.News.Add(n);
+            db.SaveChanges();
+        }
+        public List<News> GetAll()
+        {
+            return db.News.ToList();
+        }
+
+        public News GetNews(int id)
+        {
+            return db.News.Find(id);
+        }
+
+        public void Delete(int id)
+        {
+           var n = GetNews(id);
+            db.News.Remove(n);
+            db.SaveChanges();
+        }
+
+        public  void Update(News n)
+        {
+            var existing = GetNews(n.Id);
+            db.Entry(existing).CurrentValues.SetValues(n);
+            db.SaveChanges();
+        }
+
+        //API Features :
+
+        public List<News>NewsByTitle(string title)
+        {
+            var ret = (from n in db.News
+                       where n.Title.Contains(title)
+                       select n).ToList();
+            return ret;
+        }
+        public List<News> NewsByCategory(string category)
+        {
+            var ret = (from n in db.News
+                       where n.Category.Contains(category)
+                       select n).ToList();
+            return ret;
+        }
+        public List<News> NewsByDate(DateTime date)
+        {
+            var ret = (from n in db.News
+                       where n.Date == date
+                       select n).ToList();
+            return ret;
+        }
+        public List<News> NewsByDateCat(DateTime date, string category)
+        {
+            var ret = (from n in db.News
+                       where n.Date == date && n.Category.Contains(category)
+                       select n).ToList();
+            return ret;
+        }
+        public List<News> NewsByDateTitle(DateTime date, string title)
+        {
+            var ret = (from n in db.News
+                       where n.Title.Contains(title) && n.Date == date
+                       select n).ToList();
+            return ret;
+        }
+    }
+}
